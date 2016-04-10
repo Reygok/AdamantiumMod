5ee39c2ffb230a73bd59db0522d70f487e25fbd1
Reygok <reygok3@gmail.com>
2016-04-10 15:06:02 +0200

    commit so others can see source and help with smp problems

--- src/main/java/com/reygok/adamantiummod/AdamantiumGenerator.java
+++ src/main/java/com/reygok/adamantiummod/AdamantiumGenerator.java
@@ -11,39 +11,34 @@ import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
-public class AdamantiumGenerator implements IWorldGenerator {
+public class AdamantiumGenerator implements IWorldGenerator
+{
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-			IChunkProvider chunkProvider) {
-		switch (world.provider.getDimension()) {
+			IChunkProvider chunkProvider)
+	{
+		switch (world.provider.getDimension())
+		{
 		case -1:
-			if (random.nextInt(2) == 0) {
+//			if (random.nextInt(2) == 0)
+//			{
 				generateNether(world, random, chunkX * 16, chunkZ * 16);
-			}
-			break;	
+//			}
+			break;
 		case 0:
-			generateSurface(world, random, chunkX * 16, chunkZ * 16);
 			break;
 		case 1:
-			generateEnd(world, random, chunkX * 16, chunkZ * 16);
 			break;
 		}
 	}
 
-	private void generateEnd(World world, Random random, int i, int j) {
-
-	}
-
-	private void generateSurface(World world, Random random, int i, int j) {
-
-	}
-
-	private void generateNether(World world, Random random, int i, int j) {
+	private void generateNether(World world, Random random, int i, int j)
+	{
 		int AdamantiumOreXCoord = i + random.nextInt(16);
-		int AdamantiumOreYCoord = random.nextInt(10);
+		int AdamantiumOreYCoord = 8 + random.nextInt(2);
 		int AdamantiumOreZCoord = j + random.nextInt(16);
 		BlockPos blockpos = new BlockPos(AdamantiumOreXCoord, AdamantiumOreYCoord, AdamantiumOreZCoord);
-		WorldGenMinable worldGenMinable = new WorldGenMinable(AdamantiumMod.vibraniumOre.getDefaultState(), 10,
+		WorldGenMinable worldGenMinable = new WorldGenMinable(AdamantiumMod.vibraniumOre.getDefaultState(), 30,
 				BlockMatcher.forBlock(Blocks.netherrack));
 		worldGenMinable.generate(world, random, blockpos);
 	}
